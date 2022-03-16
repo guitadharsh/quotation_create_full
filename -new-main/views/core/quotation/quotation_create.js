fb269852640f7441b7467f72529bcd55e2840f2d
Adharsh D <adharshd100@gmail.com>
2022-03-16 15:01:11 +0530

    Radio buttons and plus symbol created

--- -new-main/views/core/quotation/quotation_create.js
+++ -new-main/views/core/quotation/quotation_create.js
@@ -1,24 +1,35 @@
-
-            var modal = document.getElementById("myModal");
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+            
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
             
 
-            var btn = document.getElementById("myBtn");
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
             
-      
-            var span = document.getElementsByClassName("close")[0];
-            
-            btn.onclick = function() {
-              modal.style.display = "block";
-            }
-            
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
 
-            span.onclick = function() {
-              modal.style.display = "none";
-            }
-            
-            window.onclick = function(event) {
-              if (event.target == modal) {
-                modal.style.display = "none";
-              }
-            }
+
+function multiply(){
+  
+  var price = document.getElementById("price").value
+  var qty = document.getElementById("qty").value
+  var form_need = document.getElementById("form_need").value
+  var m = price*qty; //to find subtotal
+  var t = (m*form_need)/100; //to find total amount
+  document.getElementById("subTotal").value = t;
+  document.getElementById("total").value = t;
+  // alert(form_need);
+
+}
+
+
             
